7cbc37bc48ceeb069f266a6d9b66eca08dc7e948
karolinasea <46023630+karolinasea@users.noreply.github.com>
2019-07-18 13:38:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
-
-
 const express = require('express'),
 http = require('http'),
 app = express(),
 server = http.createServer(app),
 io = require('socket.io').listen(server);
+
 app.get('/', (req, res) => {
 
 res.send('Chat Server is running on port 3000')
 });
+
+var userList = [];
+
 io.on('connection', (socket) => {
 
 console.log('user connected')
 
+
 socket.on('join', function(userNickname) {
 
         console.log(userNickname +" : has joined the chat "  );
-
         socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
+        
+        var userInfo = {};
+      	var foundUser = false;
+      	for (var i=0; i<userList.length; i++) {
+        if (userList[i]["nickname"] == userNickname) {
+          userList[i]["isConnected"] = true
+          // userList[i]["id"] = clientSocket.id;
+          userInfo = userList[i];
+          foundUser = true;
+          break;
+        }
+      }
+
+      if (!foundUser) {
+//         userInfo["id"] = clientSocket.id;
+        userInfo["nickname"] = userNickname;
+        userInfo["isConnected"] = true
+        userList.push(userInfo);
+      }
+
+      io.emit("userList", userList);
+      io.emit("connectedUsersList", userList);
+      io.emit("userConnectUpdate", userInfo)
+      
+      //afficher list des user sur la console
+      for (var i=0; i<userList.length; i++) {
+      console.log(userList[i]["nickname"] + " " + userList[i]["isConnected"]);
+      }
+      
     })
 
 
@@ -39,22 +70,14 @@ socket.on('messagedetection', (senderNickname,messageContent) => {
 
 socket.on('disconnect', function() {
 
- //        console.log(userNickname +' has left ')
-// 
-//         socket.broadcast.emit( "userdisconnect" ,' user has left')
+        // console.log(userNickname +' has left ')
+        console.log('one user has left ')
 
-
-
+        socket.broadcast.emit( "userdisconnect" ,' user has left')
 
     })
-
-
-
-
+sendTo(socket, userList);
 })
-
-
-
 
 
 
@@ -64,3 +87,6 @@ console.log('Node app is running on port 3000')
 
 })
 
+function sendTo(connection, message) { 
+   connection.send(JSON.stringify(message)); 
+}
