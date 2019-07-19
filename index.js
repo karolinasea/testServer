873d4eac3cd248f0d844c0ad447615002e8e1c75
karolinasea <46023630+karolinasea@users.noreply.github.com>
2019-07-19 08:48:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,92 +4,89 @@ app = express(),
 server = http.createServer(app),
 io = require('socket.io').listen(server);
 
-app.get('/', (req, res) => {
-
-res.send('Chat Server is running on port 3000')
+app.get('/', (req, res) => 
+{
+  res.send('Chat Server is running on port 3000')
 });
 
 var userList = [];
 
-var usersDataBase = {
-//  	users = [];
-	}; 
+var usersDataBase = 
+{
+  //  	users = [];
+}; 
 	
 
-io.on('connection', (socket) => {
-
-console.log('user connected')
-
-
-socket.on('join', function(userNickname) 
+io.on('connection', (socket) => 
 {
+      console.log('user connected')
 
-        console.log(userNickname +" : has joined the chat "  );
-        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
-        
-        var userInfo = {};
-      	var foundUser = false;
-      	for (var i=0; i<userList.length; i++) 
-      	{
-        	if (userList[i]["nickname"] == userNickname) 
-       	    {
-          		userList[i]["isConnected"] = true
-          		userInfo = userList[i];
-          		foundUser = true;
-          		break;
-        	}
-        }	
-
-       if (!foundUser) 
-       {
-        	userInfo["nickname"] = userNickname;
-        	userInfo["isConnected"] = true
-        	userList.push(userInfo);
-        }
-
-      io.emit("userList", userList);
-      io.emit("connectedUsersList", userList);
-      io.emit("userConnectUpdate", userInfo)
-      
-      //afficher list des user sur la console
-      for (var i=0; i<userList.length; i++) 
+      socket.on('join', function(userNickname) 
       {
-      	console.log(userList[i]["nickname"] + " " + userList[i]["isConnected"]);
-      }
-      
-      socket.on('disconnect', function() 
-      {
-      	 console.log(userNickname +' has left ')
-        for (var i=0; i<userList.length; i++) 
-        {
-        	if (userList[i]["nickname"] == userNickname) 
-        	{
-          		userList[i]["isConnected"] = false
-          		break;
-          	}
-        }
-		io.emit("userList", userList);
-        socket.broadcast.emit( "userdisconnect" ,' user has left')
-    })
-      
-    })
+            console.log(userNickname +" : has joined the chat "  );
+            socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
+                  
+            var userInfo = {};
+            var foundUser = false;
+            for (var i=0; i<userList.length; i++) 
+            {
+              if (userList[i]["nickname"] == userNickname) 
+              {
+                userList[i]["isConnected"] = true
+                userInfo = userList[i];
+                foundUser = true;
+                break;
+              }
+            }	
+
+            if (!foundUser) 
+            {
+              userInfo["nickname"] = userNickname;
+              userInfo["isConnected"] = true
+              userList.push(userInfo);
+            }
+
+            io.emit("userList", userList);
+            io.emit("connectedUsersList", userList);
+            io.emit("userConnectUpdate", userInfo)
+                
+            //afficher list des user sur la console
+            for (var i=0; i<userList.length; i++) 
+            {
+              console.log(userList[i]["nickname"] + " " + userList[i]["isConnected"]);
+            }
+                
+            socket.on('disconnect', function() 
+            {
+              console.log(userNickname +' has left ')
+              for (var i=0; i<userList.length; i++) 
+              {
+                if (userList[i]["nickname"] == userNickname) 
+                {
+                  userList[i]["isConnected"] = false
+                  break;
+                }
+              }
+          		
+            io.emit("userList", userList);
+            
+            socket.broadcast.emit( "userdisconnect" ,' user has left')
+      });   
+})
 
 
-socket.on('messagedetection', (senderNickname,messageContent) => {
-
+socket.on('messagedetection', (senderNickname,messageContent) => 
+{
        //log the message in console 
-
        console.log(senderNickname+" : " +messageContent)
 
       //create a message object 
-
       let  message = {"message":messageContent, "senderNickname":senderNickname}
 
        // send the message to all users including the sender  using io.emit() 
-
       io.emit('message', message )
 
-      })
+})
       
 socket.on('newUser', (newUserName, newPassword) => 
 {
@@ -100,39 +97,49 @@ socket.on('newUser', (newUserName, newPassword) =>
 		var json = JSON.stringify(newObject); 
 		var fs = require('fs');
 // 		fs.writeFile('myjsonfile.json', json, 'utf8', callback);
-		fs.readFile('usersDataBase.json', 'utf8', function readFileCallback(err, data){
-		if (err){
-			console.log(err);
-		} 
-		else {
-			usersDataBase = JSON.parse(data);
-			
-			usersDataBase.users.push(newObject);
-			
-			json = JSON.stringify(usersDataBase); 
-			
-			fs.writeFile('usersDataBase.json', json, 'utf8',function (err) {
-		   	if (err) {
-			return console.log(err);
-		    	}
-			else {
-		    	console.log("The file was saved!"); // write it back 
-			}
-}});
+		fs.readFile('usersDataBase.json', 'utf8', function readFileCallback(err, data)
+    {
+    		if (err)
+        {
+    			console.log(err);
+    		} 
+    		else 
+        {
+    			usersDataBase = JSON.parse(data);
+    			
+    			usersDataBase.users.push(newObject);
+    			
+    			json = JSON.stringify(usersDataBase); 
+    			
+    			fs.writeFile('usersDataBase.json', json, 'utf8',function (err) 
+    			{
+    		   		if (err) 
+              {
+    				    return console.log(err);
+    		    	}
+    				  else 
+              {
+    		    		console.log("The file was saved!"); // write it back 
+    					}
+		      })
+        }
+      })
+	})
 
-sendTo(socket, userList);
+  sendTo(socket, userList);
 })
 
-}
-)
 
-
-server.listen(3000,()=>{
-
-console.log('Node app is running on port 3000')
-
+server.listen(3000,()=>
+{
+  console.log('Node app is running on port 3000')
 })
 
-function sendTo(connection, message) { 
+function sendTo(connection, message) 
+{ 
    connection.send(JSON.stringify(message)); 
 }
+
+
+
+
